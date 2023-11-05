d2a4e3b04cdd87e0b411fbda778ac7e1ed04b0ad
Lucas Aguilar <lucasaguilar835@gmail.com>
2023-11-05 20:33:33 -0300

    Testes unitarios de Quarto

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   root to: 'home#home'
   
   get   'my_inn',                    to: 'inns#my_inn',                  as: 'my_inn'
-  patch 'my_inn/status',             to: 'inns#change_status',           as: 'inn_change_status'
+  patch 'my_inn/status',             to: 'inns#change_status',           as: 'status_inn'
   get   'my_inn/edit',               to: 'inns#edit',                    as: 'edit_inn'
+  # get   'my_inn/aditional/edit/:id', to: 'inn_additional#edit',          as: 'edit_inn_additional'
   get   'my_inn/aditional/edit/:id', to: 'additional_informations#edit', as: 'edit_additional_information'   
   
   resources :inns, only: [:new, :create, :show, :update]  
   resources :additional_informations, only: [:update]
   resources :payment_methods, only: [:new, :create, :edit, :update, :destroy]
   resources :rooms, only: [:new, :create, :edit, :update] do
-    patch 'status', to: 'rooms#change_status', as: 'change_status'
+    patch 'status', to: 'rooms#change_status', as: 'status'
   end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,4 +1,103 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  describe '#valid?' do
+    context 'presence' do
+      it 'false when name is empty' do
+        # Arrange
+        user = User.create!(name: 'Lucas', email: 'lucas@gmail.com', password: '123456', innkeeper: true)
+        address = Address.new(street: 'Rua dos Bobos, 115', neighborhood: 'Vila Madalena', 
+                              state: 'SP', city: 'São Paulo', zip_code: '05412000')
+        inn = Inn.create!(name: 'Pousada do Alemão', social_name: 'Pousada do Alemão LTDA', 
+                          cnpj: '12345678901234', phone: '11999999999', email: 'pdalemao@gmail.com', 
+                          address: address, user: user)
+        room = Room.new(name: '', description: 'Quarto com vista para o mar', dimension: 20,
+                    max_occupancy: 2, value: 200, inn: inn)
+        # Act
+        result = room.valid? 
+        # Assert
+        expect(result).to eq false
+      end
+      
+      it 'false when description is empty' do
+        # Arrange
+        user = User.create!(name: 'Lucas', email: 'lucas@gmail.com', password: '123456', innkeeper: true)
+        address = Address.new(street: 'Rua dos Bobos, 115', neighborhood: 'Vila Madalena', 
+                              state: 'SP', city: 'São Paulo', zip_code: '05412000')
+        inn = Inn.create!(name: 'Pousada do Alemão', social_name: 'Pousada do Alemão LTDA', 
+                          cnpj: '12345678901234', phone: '11999999999', email: 'pdalemao@gmail.com', 
+                          address: address, user: user)
+        room = Room.new(name: 'Quarto 1', description: '', dimension: 20,
+                    max_occupancy: 2, value: 200, inn: inn)
+        # Act
+        result = room.valid? 
+        # Assert
+        expect(result).to eq false
+      end
+      
+      it 'false when dimension is empty' do
+        # Arrange
+        user = User.create!(name: 'Lucas', email: 'lucas@gmail.com', password: '123456', innkeeper: true)
+        address = Address.new(street: 'Rua dos Bobos, 115', neighborhood: 'Vila Madalena', 
+                              state: 'SP', city: 'São Paulo', zip_code: '05412000')
+        inn = Inn.create!(name: 'Pousada do Alemão', social_name: 'Pousada do Alemão LTDA', 
+                          cnpj: '12345678901234', phone: '11999999999', email: 'pdalemao@gmail.com', 
+                          address: address, user: user)
+        room = Room.new(name: 'Quarto 1', description: 'Quarto com vista para o mar', dimension: nil,
+                    max_occupancy: 2, value: 200, inn: inn)
+        # Act
+        result = room.valid? 
+        # Assert
+        expect(result).to eq false
+      end
+      
+      it 'false when max_occupancy is empty' do
+        # Arrange
+        user = User.create!(name: 'Lucas', email: 'lucas@gmail.com', password: '123456', innkeeper: true)
+        address = Address.new(street: 'Rua dos Bobos, 115', neighborhood: 'Vila Madalena', 
+                              state: 'SP', city: 'São Paulo', zip_code: '05412000')
+        inn = Inn.create!(name: 'Pousada do Alemão', social_name: 'Pousada do Alemão LTDA', 
+                          cnpj: '12345678901234', phone: '11999999999', email: 'pdalemao@gmail.com', 
+                          address: address, user: user)
+        room = Room.new(name: 'Quarto 1', description: 'Quarto com vista para o mar', dimension: 12,
+                    max_occupancy: 2, value: nil, inn: inn)
+        # Act
+        result = room.valid? 
+        # Assert
+        expect(result).to eq false
+      end
+      
+      it 'false when value is empty' do
+        # Arrange
+        user = User.create!(name: 'Lucas', email: 'lucas@gmail.com', password: '123456', innkeeper: true)
+        address = Address.new(street: 'Rua dos Bobos, 115', neighborhood: 'Vila Madalena', 
+                              state: 'SP', city: 'São Paulo', zip_code: '05412000')
+        inn = Inn.create!(name: 'Pousada do Alemão', social_name: 'Pousada do Alemão LTDA', 
+                          cnpj: '12345678901234', phone: '11999999999', email: 'pdalemao@gmail.com', 
+                          address: address, user: user)
+        room = Room.new(name: 'Quarto 1', description: 'Quarto com vista para o mar', dimension: 12,
+                    max_occupancy: 2, value: nil, inn: inn)
+        # Act
+        result = room.valid? 
+        # Assert
+        expect(result).to eq false
+      end
+      
+      it 'false when inn is empty' do
+        # Arrange
+        user = User.create!(name: 'Lucas', email: 'lucas@gmail.com', password: '123456', innkeeper: true)
+        address = Address.new(street: 'Rua dos Bobos, 115', neighborhood: 'Vila Madalena', 
+                              state: 'SP', city: 'São Paulo', zip_code: '05412000')
+        inn = Inn.create!(name: 'Pousada do Alemão', social_name: 'Pousada do Alemão LTDA', 
+                          cnpj: '12345678901234', phone: '11999999999', email: 'pdalemao@gmail.com', 
+                          address: address, user: user)
+        room = Room.new(name: 'Quarto 1', description: 'Quarto com vista para o mar', dimension: 12,
+                    max_occupancy: 2, value: 200, inn: nil)
+        # Act
+        result = room.valid? 
+        # Assert
+        expect(result).to eq false
+      end
+    end
+  end
 end
